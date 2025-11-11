7d3812711ef301fe2e84d12e540fdedb4dd50ac6
Ilja Stepanov <ilstep@taltech.ee>
2025-11-11 09:08:46 +0200

    Praktikum11

--- Backend/FacetApi/Migrations/20251111065940_Initial.Designer.cs
+++ Backend/FacetApi/Migrations/20251111065940_Initial.Designer.cs
@@ -12,8 +12,8 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace FacetApi.Migrations
 {
     [DbContext(typeof(FacetDbContext))]
-    [Migration("20251110110206_initialCreate")]
-    partial class initialCreate
+    [Migration("20251111065940_Initial")]
+    partial class Initial
     {
         /// <inheritdoc />
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
