894b2e5e536b5339c666c871009b196abe965436
Mark Djatšuk <madjat@taltech.ee>
2025-12-05 15:55:01 +0200

    mobile version fix v2.0

--- Frontend/app/app.vue
+++ Frontend/app/app.vue
@@ -1,8 +1,3 @@
 <template>
-	<Head>
-		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=5, user-scalable=yes, viewport-fit=cover" />
-		<meta name="format-detection" content="telephone=no" />
-	</Head>
 	<NuxtPage />
-  
 </template>
